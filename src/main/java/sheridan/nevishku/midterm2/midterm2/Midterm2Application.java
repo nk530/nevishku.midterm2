021accb0cd8f5961e735b9d6be8f2920f6be0044
Nevish Jeetun <nevishku@sheridancollege.ca>
2023-06-19 08:25:07 -0400

    First Commit

---
+++ src/main/java/sheridan/nevishku/midterm2/midterm2/Midterm2Application.java
@@ -0,0 +1,13 @@
+package sheridan.nevishku.midterm2.midterm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Midterm2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Midterm2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/nevishku/midterm2/midterm2/Midterm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.nevishku.midterm2.midterm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Midterm2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
